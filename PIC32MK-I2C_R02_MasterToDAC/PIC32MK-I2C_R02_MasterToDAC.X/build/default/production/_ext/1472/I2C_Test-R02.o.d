1b6ca413b4610901f71652664e1acf8ddc14d8d7
lelisele <info@electronics-lis.com>
2021-12-28 16:43:53 +0100

    Add files via upload

---
+++ PIC32MK-I2C_R02_MasterToDAC/PIC32MK-I2C_R02_MasterToDAC.X/build/default/production/_ext/1472/I2C_Test-R02.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1472/I2C_Test-R02.o: ../I2C_Test-R02.c \
+ C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h
+
+C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h:
---
+++ PIC32MK-PWM-R03/PIC32MK-PWM-R03_Test.X/build/default/production/_ext/1472/PWM-Centred-R01_Test.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1472/PWM-Centred-R01_Test.o:  \
+ ../PWM-Centred-R01_Test.c  \
+
