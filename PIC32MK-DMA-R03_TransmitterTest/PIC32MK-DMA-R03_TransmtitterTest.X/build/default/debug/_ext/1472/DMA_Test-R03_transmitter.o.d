d94e5d3964c4a54491da4b0c7f39209b26c4b683
lelisele <info@electronics-lis.com>
2022-01-06 22:57:18 +0100

    Delete PIC32MK-DMA-R03_TransmitterTest directory

--- PIC32MK-DMA-R03_TransmitterTest/PIC32MK-DMA-R03_TransmtitterTest.X/build/default/debug/_ext/1472/DMA_Test-R03_transmitter.o.d
+++
@@ -1,5 +0,0 @@
-build/default/debug/_ext/1472/DMA_Test-R03_transmitter.o: \
- ../DMA_Test-R03_transmitter.c \
- C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h
-
-C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h:
