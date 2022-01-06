6e7632c0688005e86d6d4103766dffaf3fccc395
lelisele <info@electronics-lis.com>
2022-01-06 22:57:02 +0100

    Delete PIC32MK-DMA-R03_ReceiverTest directory

--- PIC32MK-DMA-R03_ReceiverTest/PIC32MK-DMA-R03_ReceiverTest.X/build/default/production/_ext/1472/DMA_Test-R03_receiver.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1472/DMA_Test-R03_receiver.o: \
- ../DMA_Test-R03_receiver.c \
- C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h
-
-C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h:
