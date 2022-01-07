1a4d3080286bd04baab5fb9492ccd86893e6b1e1
electronics-lis <info@electronics-lis.com>
2022-01-07 13:26:53 +0100

    Add files via upload

---
+++ PIC32MK-DMA-UART-R04/PIC32MK-DMA-UART-R04.X/build/default/production/_ext/1472/PIC32MK-DMA-UART-R03.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/PIC32MK-DMA-UART-R03.o: \
+ ../PIC32MK-DMA-UART-R03.c \
+ C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h
+
+C:\Program\ Files\Microchip\xc32\v2.50\pic32mx\include\proc\PIC32MK-MC\p32mk1024mcf064.h:
